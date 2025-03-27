b13f5427c549a61d9e9a8185c3cee0723d15a974
Naman Chandok <namanchandok1@gmail.com>
2025-03-27 15:53:27 +0530

    trial p3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['mui-file-input'],
+	transpilePackages: ["mui-file-input"],
+	env: {
+		API_KEY: "AIzaSyC9B4W5sVjEQ7fnsWA8MEVWAs2j5gj4raw",
+		USER_EMAIL: "acmuser@yopmail.com",
+	},
 };
 
 export default nextConfig;
